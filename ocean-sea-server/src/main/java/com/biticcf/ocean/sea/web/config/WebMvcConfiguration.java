bc31da06b4791f4be0cea8f3417501ee86ed88f8
biticcf <19070443@qq.com>
2020-04-23 14:01:50 +0800

    添加默认的静态文件路径,防止被串改

--- ocean-sea-server/src/main/java/com/biticcf/ocean/sea/web/config/WebMvcConfiguration.java
+++ ocean-sea-server/src/main/java/com/biticcf/ocean/sea/web/config/WebMvcConfiguration.java
@@ -23,6 +23,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.alibaba.druid.support.http.StatViewServlet;
@@ -132,5 +133,12 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
         
         return filterRegistrationBean;
+    }
+    
+    // +系统默认静态文件路径,优先级比config文件高
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	registry.addResourceHandler("/**")
+    	        .addResourceLocations("classpath:/META-INF/resources/", "classpath:/resources/", "classpath:/static/", "classpath:/public/");
     }
 }
